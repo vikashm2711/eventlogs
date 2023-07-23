21f198a04f40571b4617b59adc6fcc1c46e0794a
vikashm2711 <vikashm2711@gmail.com>
2023-07-23 19:03:33 +0530

    update json

--- eventhubnamespace.ps1
+++ eventhubnamespace.ps1
@@ -1,10 +1,10 @@
 # Parameters
-$location = "YOUR_LOCATION"           # Replace with your desired location
-$namespaceName = "YOUR_NAMESPACE"     # Replace with your desired namespace name
-$resourceGroupName = "YOUR_RG_NAME"   # Replace with your desired resource group name
-$skuName = "YOUR_SKU_NAME"            # Replace with your desired SKU name
-$skuTier = "YOUR_SKU_TIER"            # Replace with your desired SKU tier
-$skuCapacity = "YOUR_SKU_CAPACITY"    # Replace with your desired SKU capacity
+$location = "eastus"           # Replace with your desired location
+$name = "motadataeventhubnamespaceps1"     # Replace with your desired namespace name
+$resourceGroupName = "motadataps1"   # Replace with your desired resource group name
+$skuName = "Standard"            # Replace with your desired SKU name
+$skuTier = "Standard"            # Replace with your desired SKU tier
+$skuCapacity = "1"    # Replace with your desired SKU capacity
 $isAutoInflateEnabled = $true         # Replace with your desired auto-inflate setting
 $maximumThroughputUnits = "100"       # Replace with your desired maximum throughput units
 $zoneRedundant = $false               # Replace with your desired zone redundancy setting
@@ -13,18 +13,17 @@ $disableLocalAuth = $false            # Replace with your desired local auth set
 $publicNetworkAccess = "Enabled"      # Replace with your desired public network access setting
 
 # Authenticate with Azure
-Connect-AzAccount
-
-# Set the context to the desired subscription (if you have multiple subscriptions)
-Set-AzContext -Subscription "YOUR_SUBSCRIPTION_ID"   # Replace with your subscription ID
-
+# Connect-AzAccount
+# Set the Azure context to the desired subscription
+@$SubscriptionId = "5807cfb0-41a6-4da6-b920-71d934d4a2af"
+Set-AzContext -SubscriptionId $SubscriptionId
 # Create a new resource group if it doesn't exist
-New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
+# New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
 
 # Deploy the Event Hub namespace using the template with parameter values
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
     -TemplateUri "https://raw.githubusercontent.com/vikashm2711/eventlogs/main/event-hub.json" `
-    -namespaceName $namespaceName -location $location `
+    -name $name -location $location `
     -skuName $skuName -skuTier $skuTier -skuCapacity $skuCapacity `
     -isAutoInflateEnabled $isAutoInflateEnabled -maximumThroughputUnits $maximumThroughputUnits `
     -zoneRedundant $zoneRedundant -minimumTlsVersion $minimumTlsVersion `
