0e76dff5abb9172a2aca7d6a7f0ee1fce4fc51c5
vikashm2711 <vikashm2711@gmail.com>
2023-07-23 18:52:47 +0530

    main

--- eventhubnamespace.ps1
+++ eventhubnamespace.ps1
@@ -1,105 +1,31 @@
-# Define the parameters for Event Hub namespace deployment
-$namespaceName = "motadataeventhubnamespaceps1"
-$location = "eastus"
-$skuName = "Standard"
-$skuTier = "Standard"
-$skuCapacity = "1"
-$isAutoInflateEnabled = "false"
-$maximumThroughputUnits = "0"
-$zoneRedundant = $true
-$minimumTlsVersion = "1.2"
-$disableLocalAuth = $false
-$publicNetworkAccess = "Enabled"
-$tags = @{}  # Empty tags as per the provided template
+# Parameters
+$location = "YOUR_LOCATION"           # Replace with your desired location
+$namespaceName = "YOUR_NAMESPACE"     # Replace with your desired namespace name
+$resourceGroupName = "YOUR_RG_NAME"   # Replace with your desired resource group name
+$skuName = "YOUR_SKU_NAME"            # Replace with your desired SKU name
+$skuTier = "YOUR_SKU_TIER"            # Replace with your desired SKU tier
+$skuCapacity = "YOUR_SKU_CAPACITY"    # Replace with your desired SKU capacity
+$isAutoInflateEnabled = $true         # Replace with your desired auto-inflate setting
+$maximumThroughputUnits = "100"       # Replace with your desired maximum throughput units
+$zoneRedundant = $false               # Replace with your desired zone redundancy setting
+$minimumTlsVersion = "1.2"            # Replace with your desired minimum TLS version
+$disableLocalAuth = $false            # Replace with your desired local auth setting
+$publicNetworkAccess = "Enabled"      # Replace with your desired public network access setting
 
-# Define your subscription ID
-$SubscriptionId = "5807cfb0-41a6-4da6-b920-71d934d4a2af"
+# Authenticate with Azure
+Connect-AzAccount
 
-# Set the Azure context to the desired subscription
-Set-AzContext -SubscriptionId $SubscriptionId
+# Set the context to the desired subscription (if you have multiple subscriptions)
+Set-AzContext -Subscription "YOUR_SUBSCRIPTION_ID"   # Replace with your subscription ID
 
-# Set the name of the resource group where you want to deploy the Event Hub namespace
-$resourceGroupName = "motadataps1"
+# Create a new resource group if it doesn't exist
+New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
 
-# Construct the parameters hashtable with explicit types
-$parametersHashTable = @{
-    "parameters" = @{
-        "name" = @{
-            "type" = "String"
-            "value" = $namespaceName
-        }
-        "location" = @{
-            "type" = "String"
-            "value" = $location
-        }
-        "skuName" = @{
-            "type" = "String"
-            "value" = $skuName
-        }
-        "skuTier" = @{
-            "type" = "String"
-            "value" = $skuTier
-        }
-        "skuCapacity" = @{
-            "type" = "String"
-            "value" = $skuCapacity
-        }
-        "isAutoInflateEnabled" = @{
-            "type" = "String"
-            "value" = $isAutoInflateEnabled
-        }
-        "maximumThroughputUnits" = @{
-            "type" = "String"
-            "value" = $maximumThroughputUnits
-        }
-        "zoneRedundant" = @{
-            "type" = "Bool"
-            "value" = $zoneRedundant
-        }
-        "minimumTlsVersion" = @{
-            "type" = "String"
-            "value" = $minimumTlsVersion
-        }
-        "disableLocalAuth" = @{
-            "type" = "Bool"
-            "value" = $disableLocalAuth
-        }
-        "publicNetworkAccess" = @{
-            "type" = "String"
-            "value" = $publicNetworkAccess
-        }
-        "tags" = @{
-            "type" = "Object"
-            "value" = $tags
-        }
-    }
-    "contentVersion" = "1.0.0.0"
-    "schema" = "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#"
-}
-
-# Convert hashtable to JSON string
-$jsonString = $parametersHashTable | ConvertTo-Json -Depth 100
-
-Add-Type -AssemblyName System.Web.Extensions
-$serializer = New-Object System.Web.Script.Serialization.JavaScriptSerializer
-$convertedHashtable = $serializer.DeserializeObject($jsonString)
-
-
-Write-Host "Converted Hashtable:"
-$convertedHashtable
-
-
-# Set the URI for the deployment template
-$deploymentUri = "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#"
-
-# Create the deployment command
-$deploymentCommand = @{
-    "Name" = "EventHubNamespaceDeployment"
-    "ResourceGroupName" = $resourceGroupName
-    "TemplateUri" = $deploymentUri
-    "TemplateParameterObject" = $convertedHashtable.parameters 
-    "Verbose" = $true
-}
-
-# Execute the deployment command
-New-AzResourceGroupDeployment @deploymentCommand
+# Deploy the Event Hub namespace using the template with parameter values
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -TemplateUri "https://raw.githubusercontent.com/vikashm2711/eventlogs/main/event-hub.json" `
+    -namespaceName $namespaceName -location $location `
+    -skuName $skuName -skuTier $skuTier -skuCapacity $skuCapacity `
+    -isAutoInflateEnabled $isAutoInflateEnabled -maximumThroughputUnits $maximumThroughputUnits `
+    -zoneRedundant $zoneRedundant -minimumTlsVersion $minimumTlsVersion `
+    -disableLocalAuth $disableLocalAuth -publicNetworkAccess $publicNetworkAccess `
